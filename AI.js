18c397cb3328a50e3e15ef178a81f980f793d227
Ben Schoelkopf <thebenschoel@gmail.com>
2020-05-17 12:22:49 -0400

    regressor working

--- AI.js
+++ AI.js
@@ -38,19 +38,56 @@ const AI = (state) => {
     state.update(
         "networkParams",
         (() => {
-            let pseudo = {};
-            pseudo.weights = [
-                [arrayOfRandom(3), arrayOfRandom(3), arrayOfRandom(3)],
-                [arrayOfRandom(3), arrayOfRandom(3)],
-            ];
-            pseudo.biases = [0, 0];
-            return pseudo;
+            return {
+                w: 1,
+                b: 1,
+            };
         })()
     );
 
-    const run = (point) => {};
+    const run = (x) => {
+        let { w, b } = state.get("networkParams");
+        return x * w + b;
+    };
 
-    const train = () => {};
+    const train = () => {
+        let epochs = 500;
+        let lr = 0.03;
+
+        for (let e = 1; e <= epochs; e++) {
+            let { w, b } = state.get("networkParams");
+            let wnudge = 0;
+            let bnudge = 0;
+
+            let points = [...state.get("points")]
+                .sort(() => 0.5 - Math.random())
+                .slice(0, 10);
+
+            for (const point of points) {
+                let actual = point.y;
+                let z = run(point.x);
+
+                let cost = Math.pow(z - actual, 2);
+                let dcost_dw = 2 * (z - actual) * point.x;
+                let dcost_db = 2 * (z - actual);
+
+                wnudge += dcost_dw;
+                bnudge += dcost_db;
+            }
+
+            w = w - wnudge * lr;
+            b = b - bnudge * lr;
+            state.update("networkParams", { w, b });
+        }
+        console.log("done training");
+        console.log(
+            "error of " +
+                [...state.get("points")].reduce((err, point) => {
+                    err += Math.pow(run(point.x) - point.y, 2);
+                    return err;
+                }, 0)
+        );
+    };
 
     return { run, train };
 };
--- main.js
+++ main.js
@@ -7,12 +7,24 @@ const els = {
 
 const state = State({
     points: [],
-    networkParams: {},
 });
 const ai = AI(state);
 
 const updateSurface = (params) => {
+    document.querySelectorAll(".marker").forEach((marker) => marker.remove());
     let rect = els.surface.getBoundingClientRect();
+
+    for (let i = 0; i < rect.width; i += 2) {
+        let m = document.createElement("div");
+        m.classList.add("marker");
+        m.style.position = "absolute";
+        m.style.left = i + "px";
+        m.style.bottom = ai.run(i / rect.width) * 100 + "%";
+        m.style.height = "2px";
+        m.style.width = "2px";
+        m.style.backgroundColor = "dodgerblue";
+        els.surface.appendChild(m);
+    }
 };
 
 // ---------- LISTENERS ----------
@@ -26,17 +38,14 @@ state.addListener("points", (points) => {
     for (const point of points) {
         let el = document.createElement("div");
         el.classList.add("point");
-        el.style.bottom = point.y + "px";
-        el.style.left = point.x + "px";
+        el.style.bottom = point.y * 100 + "%";
+        el.style.left = point.x * 100 + "%";
         el.style.backgroundColor = "#f6a5ad";
         els.surface.appendChild(el);
     }
 });
 
 state.addListener("networkParams", updateSurface);
-state.addListener("isShowingMarkers", () => {
-    updateSurface(state.get("networkParams"));
-});
 
 els.surface.addEventListener("click", (e) => {
     let rect = els.surface.getBoundingClientRect();
@@ -44,12 +53,12 @@ els.surface.addEventListener("click", (e) => {
     let y = rect.height - (e.pageY - rect.top);
     let points = [...state.get("points")];
     points.push({
-        x,
-        y,
+        x: x / rect.width,
+        y: y / rect.height,
     });
     state.update("points", points);
 });
 
-document
-    .querySelector("#train-btn")
-    .addEventListener("click", () => ai.train());
+document.querySelector("#train-btn").addEventListener("click", () => {
+    ai.train();
+});
