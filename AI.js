608443e6a56048f0458901d0edfd680d2418fead
Ben Schoelkopf <thebenschoel@gmail.com>
2020-05-17 22:37:26 -0400

    good effort

--- AI.js
+++ AI.js
@@ -26,70 +26,271 @@ const arrayOfRandom = (len) => {
     return arr;
 };
 
-const sigmoid = (x) => 1 / (1 + Math.exp(-x));
-
-const sigmoid_p = (x) => Math.exp(-x) / Math.pow(1 + Math.exp(-x), 2);
-
-const relu = (x) => Math.max(x, 0);
-
-const relu_p = (x) => (x < 0 ? 0 : 1);
-
-const AI = (state) => {
-    state.update(
-        "networkParams",
-        (() => {
-            return {
-                w: 1,
-                b: 1,
-            };
-        })()
-    );
-
-    const run = (x) => {
-        let { w, b } = state.get("networkParams");
-        return x * w + b;
+const getActivationFunction = (key, prime = false) => {
+    const f = {
+        sigmoid: (x) => 1 / (1 + Math.exp(-x)),
+        relu: (x) => Math.max(x, 0),
     };
 
-    const train = () => {
-        let epochs = 500;
-        let lr = 0.03;
+    const p = {
+        sigmoid: (x) => Math.exp(-x) / Math.pow(1 + Math.exp(-x), 2),
+        relu: (x) => (x < 0 ? 0 : 1),
+    };
+
+    if (!prime) return f[key];
+    else return p[key];
+};
+
+const getCostFunction = (key, prime = false) => {
+    //y is what the ai predicted, yHat is the actual value
+    const f = {
+        mse: (y, yHat) => Math.pow(y - yHat, 2),
+    };
+
+    const p = {
+        mse: (y, yHat) => 2 * (y - yHat),
+    };
+
+    if (!prime) return f[key];
+    else return p[key];
+};
+
+const AI = (inputLen) => {
+    let layers = [];
+
+    const add = (size, activation) => {
+        /*
+            Layer Schema
+            size: n,
+            weights: [] with length n of arrays with length prior layer size,
+            bias: random number,
+            activation: name of activation function to use
+        */
+
+        let priorLen =
+            layers.length === 0 ? inputLen : layers[layers.length - 1].size;
+
+        let layer = { size, activation };
+        layer.bias = Math.random();
+        layer.weights = Array(size)
+            .fill(0)
+            .map((_) => arrayOfRandom(priorLen));
+
+        layers.push(layer);
+    };
+
+    const train = (training, options) => {
+        let { cost, epochs, lr } = {
+            ...{
+                cost: "mse",
+                epochs: 1000,
+                lr: 0.1,
+            },
+            ...options,
+        };
+
+        let costFunction = getCostFunction(cost);
+        let costFunction_p = getCostFunction(cost, true);
 
         for (let e = 1; e <= epochs; e++) {
-            let { w, b } = state.get("networkParams");
-            let wnudge = 0;
-            let bnudge = 0;
+            let derivs = {
+                weights: Array(layers.length)
+                    .fill(0)
+                    .map((_, index) => {
+                        let prev = layers[index - 1]
+                            ? layers[index - 1].weights
+                            : training[0].input;
 
-            let points = [...state.get("points")]
-                .sort(() => 0.5 - Math.random())
-                .slice(0, 10);
+                        return Array(layers[index].weights.length).fill(
+                            Array(prev.length).fill(0)
+                        );
+                    }),
+                biases: Array(layers.length).fill(0),
+            };
 
-            for (const point of points) {
-                let actual = point.y;
-                let z = run(point.x);
-
-                let cost = Math.pow(z - actual, 2);
-                let dcost_dw = 2 * (z - actual) * point.x;
-                let dcost_db = 2 * (z - actual);
-
-                wnudge += dcost_dw;
-                bnudge += dcost_db;
+            if (e % 100 === 0) {
+                console.log("Epoch " + e);
+                let err = 0;
+                for (const data of training) {
+                    let pred = run(data.input);
+                    let actual = data.output;
+                    err += costFunction(pred[0], actual[0]);
+                }
+                console.log(err);
             }
 
-            w = w - wnudge * lr;
-            b = b - bnudge * lr;
-            state.update("networkParams", { w, b });
+            for (const data of training) {
+                const { zs, as } = run(data.input, true);
+                let prediction = as[as.length - 1];
+                let actual = data.output;
+
+                const prevd = Array(as.length).fill([]);
+
+                for (let i = as.length - 1; i >= 0; i--) {
+                    let layer = layers[i];
+                    let activation_p = getActivationFunction(
+                        layer.activation,
+                        true
+                    );
+
+                    let aGroup = as[i];
+
+                    for (let j = 0; j < aGroup.length; j++) {
+                        let da_dz = activation_p(zs[i][j]);
+
+                        if (i === as.length - 1) {
+                            da_dz *= costFunction_p(prediction[j], actual[j]);
+                        }
+
+                        prevd[i][j] = da_dz;
+
+                        let weights = [...layer.weights];
+
+                        for (let k = 0; k < weights.length; k++) {
+                            for (let l = 0; l < weights[k].length; l++) {
+                                let prior = as[i - 1] ? as[i - 1] : data.input;
+
+                                const dz_dwn = prior[l];
+                                let da_dwn = da_dz * dz_dwn;
+
+                                let da_db = da_dz;
+
+                                if (as[i + 1]) {
+                                    for (let n = 0; n < as[i + 1].length; n++) {
+                                        const specificDcost =
+                                            da_dwn *
+                                            prevd[i + 1][n] *
+                                            layers[i + 1].weights[n][k];
+
+                                        derivs.weights[i][k][
+                                            l
+                                        ] += specificDcost;
+
+                                        derivs.biases[i] +=
+                                            da_db *
+                                            prevd[i + 1][n] *
+                                            layers[i + 1].weights[n][k];
+                                    }
+                                } else {
+                                    derivs.weights[i][k][l] += da_dwn; //this da goes all the way to top due to checking for previous derivatives
+                                    derivs.biases[i] += da_db;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            //apply calculated derivatives here
+            for (let i = 0; i < derivs.weights.length; i++) {
+                let layerWeights = derivs.weights[i];
+                for (let j = 0; j < layerWeights.length; j++) {
+                    for (let k = 0; k < layerWeights[j].length; k++) {
+                        layers[i].weights[j][k] =
+                            layers[i].weights[j][k] -
+                            derivs.weights[i][j][k] * lr;
+                    }
+                }
+            }
+
+            for (let i = 0; i < derivs.biases.length; i++) {
+                let d = derivs.biases[i];
+                layers[i].bias = layers[i].bias - d * lr;
+            }
         }
-        console.log("done training");
-        console.log(
-            "error of " +
-                [...state.get("points")].reduce((err, point) => {
-                    err += Math.pow(run(point.x) - point.y, 2);
-                    return err;
-                }, 0)
-        );
     };
 
-    return { run, train };
+    const run = (input, returnSubLayers = false) => {
+        let v = [...input];
+        let zs = [];
+        let bs = [];
+        let as = [];
+
+        for (let i = 0; i < layers.length; i++) {
+            let z = dot(v, layers[i].weights);
+            zs.push(z);
+            let b = apply(z, (n) => n + layers[i].bias);
+            bs.push(b);
+            let a = apply(b, getActivationFunction(layers[i].activation));
+            as.push(a);
+            v = [...a];
+        }
+
+        if (returnSubLayers) {
+            return {
+                zs,
+                bs,
+                as,
+            };
+        } else {
+            return v;
+        }
+    };
+
+    return {
+        add,
+        train,
+        run,
+    };
 };
+
+//---------------   REGRESSION  ---------------
+// const AI = (state) => {
+//     state.update(
+//         "networkParams",
+//         (() => {
+//             return {
+//                 w: 1,
+//                 b: 1,
+//             };
+//         })()
+//     );
+
+//     const run = (x) => {
+//         let { w, b } = state.get("networkParams");
+//         return x * w + b;
+//     };
+
+//     const train = () => {
+//         let epochs = 500;
+//         let lr = 0.03;
+
+//         for (let e = 1; e <= epochs; e++) {
+//             let { w, b } = state.get("networkParams");
+//             let wnudge = 0;
+//             let bnudge = 0;
+
+//             let points = [...state.get("points")]
+//                 .sort(() => 0.5 - Math.random())
+//                 .slice(0, 10);
+
+//             for (const point of points) {
+//                 let actual = point.y;
+//                 let z = run(point.x);
+
+//                 let cost = Math.pow(z - actual, 2);
+//                 let dcost_dw = 2 * (z - actual) * point.x;
+//                 let dcost_db = 2 * (z - actual);
+
+//                 wnudge += dcost_dw;
+//                 bnudge += dcost_db;
+//             }
+
+//             w = w - wnudge * lr;
+//             b = b - bnudge * lr;
+//             state.update("networkParams", { w, b });
+//         }
+//         console.log("done training");
+//         console.log(
+//             "error of " +
+//                 [...state.get("points")].reduce((err, point) => {
+//                     err += Math.pow(run(point.x) - point.y, 2);
+//                     return err;
+//                 }, 0)
+//         );
+//     };
+
+//     return { run, train };
+// };
 
 export default AI;
--- main.js
+++ main.js
@@ -1,64 +1,38 @@
 import State from "./state";
 import AI from "./AI";
 
-const els = {
-    surface: document.querySelector("#surface"),
-};
+//and gate
+let data = [
+    [0, 0, 0],
+    [0, 1, 0],
+    [1, 0, 0],
+    [1, 1, 1],
+];
 
-const state = State({
-    points: [],
-});
-const ai = AI(state);
-
-const updateSurface = (params) => {
-    document.querySelectorAll(".marker").forEach((marker) => marker.remove());
-    let rect = els.surface.getBoundingClientRect();
-
-    for (let i = 0; i < rect.width; i += 2) {
-        let m = document.createElement("div");
-        m.classList.add("marker");
-        m.style.position = "absolute";
-        m.style.left = i + "px";
-        m.style.bottom = ai.run(i / rect.width) * 100 + "%";
-        m.style.height = "2px";
-        m.style.width = "2px";
-        m.style.backgroundColor = "dodgerblue";
-        els.surface.appendChild(m);
-    }
-};
-
-// ---------- LISTENERS ----------
-
-state.addListener("points", (points) => {
-    let priorPoints = [...document.querySelectorAll(".point")];
-    priorPoints.forEach((pointEl) => {
-        pointEl.remove();
-    });
-
-    for (const point of points) {
-        let el = document.createElement("div");
-        el.classList.add("point");
-        el.style.bottom = point.y * 100 + "%";
-        el.style.left = point.x * 100 + "%";
-        el.style.backgroundColor = "#f6a5ad";
-        els.surface.appendChild(el);
-    }
-});
-
-state.addListener("networkParams", updateSurface);
-
-els.surface.addEventListener("click", (e) => {
-    let rect = els.surface.getBoundingClientRect();
-    let x = e.pageX - rect.left;
-    let y = rect.height - (e.pageY - rect.top);
-    let points = [...state.get("points")];
-    points.push({
-        x: x / rect.width,
-        y: y / rect.height,
-    });
-    state.update("points", points);
-});
+const state = State({});
+const ai = AI(2);
+ai.add(1, "relu");
 
 document.querySelector("#train-btn").addEventListener("click", () => {
-    ai.train();
+    let trainingData = data.map((dataGroup) => {
+        return {
+            input: dataGroup.slice(0, 2),
+            output: [dataGroup[2]],
+        };
+    });
+    ai.train(trainingData, {
+        cost: "mse",
+        epochs: 100000,
+        lr: 0.01,
+    });
+});
+
+document.querySelector("#run-btn").addEventListener("click", () => {
+    for (const point of data) {
+        console.log(point);
+        console.log(ai.run(point.slice(0, 2)));
+    }
+    // let dataPoint = data[Math.floor(Math.random() * data.length)];
+    // console.log(dataPoint);
+    // console.log(ai.run(dataPoint.slice(0, 2)));
 });
